5b3b060c47581205ce2b4d94a40edbdb3a6aa3ac
AbhiThakkar <thakkar.ab@husky.neu.edu>
2020-02-19 16:56:40 -0500

    Assign 4 Big Changes

---
+++ Assignment4/src/Business/Users/Admin.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Users;
+
+import Business.Abstract.User;
+import Business.CustomerDirectory;
+import Business.AirlinerDirectory;
+
+/**
+ *
+ * @author AEDSpring2019
+ */
+public class Admin extends User {
+    
+    public AirlinerDirectory airDir;
+    public CustomerDirectory custDir;
+    
+    public Admin() {
+        super("", "", "Admin");
+        airDir = new AirlinerDirectory();
+        custDir = new CustomerDirectory();
+    }
+
+    public AirlinerDirectory getAirDir() {
+        return airDir;
+    }
+
+    public void setAirDir(AirlinerDirectory airDir) {
+        this.airDir = airDir;
+    }
+
+    
+
+    public CustomerDirectory getCustDir() {
+        return custDir;
+    }
+
+    public void setCustDir(CustomerDirectory custDir) {
+        this.custDir = custDir;
+    }
+    
+    @Override
+    public boolean verify(String password){
+        return password.equals(getPassword());
+    }
+    
+}
